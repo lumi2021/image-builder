c9b3d3db748d0d7f00c7ff4399e0ad0cbbc6dc8a
lumi2021 <4462076+lumi2021@users.noreply.github.com>
2025-02-12 00:29:41 -0300

    file created and resized

--- build.zig
+++ build.zig
@@ -5,7 +5,10 @@ const addBuildImage = imageBuilder.AddBuildImage;
 
 pub fn build(b: *std.Build) void {
     
-    const step = addBuildImage(b, "1k", ".disk-image");
-    b.default_step.dependOn(step);
+    const step = addBuildImage(b, "5k", ".disk-image");
+
+    const build_step = b.step("b", "Ty build this shit");
+    build_step.dependOn(&step.step);
+    b.default_step = build_step;
 
 }
--- imageBuilder.zig
+++ imageBuilder.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 const Build = std.Build;
 const Step = Build.Step;
+const fs = std.fs;
 
-pub fn AddBuildImage(b: *Build, comptime size: []const u8, out_path: []const u8) *Step {
+pub fn AddBuildImage(b: *Build, comptime size: []const u8, out_path: []const u8) *ImageBuilder {
 
     comptime var str_size: []const u8 = undefined;
     var multiplier: usize = 1;
@@ -14,19 +15,17 @@ pub fn AddBuildImage(b: *Build, comptime size: []const u8, out_path: []const u8)
             'k' => multiplier = 1024,
             'm' => multiplier = 1024 * 1024,
             'g' => multiplier = 1024 * 1024 * 1024,
-            else => @compileError("unexpected size indicator " ++ size[size.len - 1])
+            else => @compileError("unexpected size indicator '" ++ size[size.len - 1..size.len] ++ "'")
         }
     } else str_size = size;
 
-    const int_size = std.fmt.parseUnsigned(usize, str_size, 16)
-    catch @panic("\"" ++ str_size ++ "\" is a not valid number!");
+    const int_size = comptime std.fmt.parseInt(usize, str_size, 16)
+    catch @compileError("\"" ++ str_size ++ "\" is a not valid hexadecimal unsigned number!");
 
-    var newStep = BuildImageStep.create(b, int_size * multiplier, out_path);
-    return &newStep.step;
-
+    return ImageBuilder.create(b, int_size * multiplier, out_path);
 }
 
-const BuildImageStep = struct {
+pub const ImageBuilder = struct {
     step: Step,
     size_bytes: usize,
     output_path: []const u8,
@@ -34,12 +33,12 @@ const BuildImageStep = struct {
     pub fn create(b: *Build, size_bytes: usize, output_path: []const u8) *@This() {
 
         const self = b.allocator.create(@This()) catch unreachable;
-
         self.* = .{
             .step = Build.Step.init(.{
                 .id = .custom,
                 .name = "Build Image",
-                .owner = b
+                .owner = b,
+                .makeFn = make
             }),
             .output_path = output_path,
             .size_bytes = size_bytes
@@ -48,12 +47,24 @@ const BuildImageStep = struct {
         return self;
 
     }
-
-    pub fn make(self: *@This(), progress: *std.Progress.Node) !void {
-        
-        progress.start("help me", 500);
-        _ = self;
-        while (true) {}
-
-    }
 };
+
+fn make(step: *Step, progress: std.Progress.Node) anyerror!void {
+    
+    const builder: *ImageBuilder = @fieldParentPtr("step", step);
+    var n = progress.start("creating image file", 1);
+
+    const img_file = fs.cwd().createFile(builder.output_path, .{}) catch unreachable;
+    defer img_file.close();
+
+    img_file.setEndPos(builder.size_bytes) catch unreachable;
+
+    n.end();
+    n = progress.start("writing directories", 1000);
+
+    // TODO write this shit
+    n.setCompletedItems(100);
+
+    n.end();
+
+}
