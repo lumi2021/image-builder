7b96e0d7c5fa78d74f965cfd527dc4bf30159fcb
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2025-05-05 01:06:23 -0300

    FUCK WHY THIS DAMN SHIT DOES NOT WORK

--- build.zig
+++ build.zig
@@ -1,17 +1,17 @@
 const std = @import("std");
-const imageBuilder = @import("imageBuilder.zig");
+const imageBuilder = @import("src/imageBuilder.zig");
 
 const addBuildGPTDiskImage = imageBuilder.addBuildGPTDiskImage;
 
 pub fn build(b: *std.Build) void {
     
-    const disk = addBuildGPTDiskImage(b, "1M", "lumiOS.img");
+    const disk = addBuildGPTDiskImage(b, "20M", "lumiOS.img");
 
-    disk.addPartitionBySectors(
+    disk.addPartitionBySize(
         .FAT,
         "Data",
         "disk-data",
-        2014 - 35
+        19 * 1024 * 1024
     );
 
     const build_step = b.step("Build", "Try to build this shit");
---
+++ src/formats/formats.zig
@@ -0,0 +1,1 @@
+pub const fat = @import("fat.zig");
